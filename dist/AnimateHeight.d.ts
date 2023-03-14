af1f953d941ef2feec8ddb301cf6bdf2ca90da42
bensondevgit <benson.song1984@outlook.com>
2023-03-14 23:11:35 +0800

    update node version

--- dist/AnimateHeight.d.ts
+++ dist/AnimateHeight.d.ts
@@ -1,14 +1,14 @@
-import type { Target, Tween } from "framer-motion";
+import type { Target, Tween } from "framer-motion/dist/framer-motion";
 import React from "react";
 interface AnimateHeightProps {
-    isVisible: boolean;
-    ease?: Tween["ease"];
-    duration?: number;
-    variants?: {
-        open: Target;
-        close: Target;
-    };
-    children: React.ReactNode;
+  isVisible: boolean;
+  ease?: Tween["ease"];
+  duration?: number;
+  variants?: {
+    open: Target;
+    close: Target;
+  };
+  children: React.ReactNode;
 }
 export declare function AnimateHeight(props: AnimateHeightProps): JSX.Element;
 export {};
